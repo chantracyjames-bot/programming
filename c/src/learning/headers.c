ac015b94aeb56f7bbdcb726693922b5bc207516b
tracy <chantracyjames@gmail.com>
2026-08-01 14:46:32 +0800

    C: Saturday Commit: Modified and corrected some typos in "headers.c" inside of "src/learning/". Added a sample code as an example.

--- c/src/learning/headers.c
+++ c/src/learning/headers.c
@@ -3,36 +3,52 @@
     - required to use features
         - like printf, scanf, string, etc.
     - libraries:
+        - errno.h
+            - enables the use of errno
+                - used for error handling
+            - syntax:
+                #include <errno.h>
+        - math.h
+            - includes the math functions and libraries
+            - enables the use of math functions
+            - syntax:
+                #include <math.h>
+        - stdbool.h
+            - includes the boolean libraries
+            - enables the use of the bool data type
+            - syntax:
+                #include <stdbool.h>
+        - stdlib.h
+            - includes most of the low-level libraries
+                - like for dynamic memory management
+            - syntax:
+                #include <stdlib.h>
         - stdio.h
             - includes the input and output libraries
             - printf for output
             - scanf for input
             - syntax:
-                - #include <stdio.h> // header declaration
-        - stdbool.h
-            - includes the boolean libraries
-            - enables the use of the bool data type
-            - syntax:
-                - #include <stdbool.h> // header declaration
-                  bool my_bool;
+                #include <stdio.h>
         - string.h
             - includes the string libraries and functions
             - enables the use of string functions
             - syntax:
-                - #include <string.h> //header declaration
-        - math.h
-            - includes the math functions and libraries
-            - enables the use of math functions
-            - syntax:
-                - #include <math.h>
-        - stdlib.h
-            - includes most of the low-level libraries
-                - like for dynamic memory management
-            - syntax:
-                - #include <stdlib.h>
-        - error.h
-            - enables the use of errno
-                - used for error handling
-            - syntax:
-                #include <errno.h>
+                #include <string.h>
 */
+
+#include <errno.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    FILE *p_file = fopen("test", "r");
+    if (p_file == NULL) {
+        printf("Error: %s", strerror(errno));
+    }
+
+    double *p_num = malloc(sizeof(double));
+    *p_num = round(1.2);
+}
