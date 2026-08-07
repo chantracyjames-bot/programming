1b16e6960c55039d478b3c11bb346d86103cfd21
tracy <chantracyjames@gmail.com>
2026-08-07 17:24:10 +0800

    SQL: Friday Commit: Added and learnt Updating in MySQL. Added learnt concepts in "updating.sql" inside "learning/tables/".

---
+++ sql/learning/tables/updating.sql
@@ -0,0 +1,11 @@
+/* updating columns */
+
+USE test;
+
+-- UPDATE <table_name>
+-- SET <column/s> = <value/s>
+-- WHERE <condition>;
+    -- updates or modifies one or more records in a table
+UPDATE yippie
+SET no = 'idkman', probs = 'lumbago'
+WHERE yes = 2;
