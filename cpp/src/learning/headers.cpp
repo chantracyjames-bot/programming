401f747289478d662133291877c45b8157171ab4
tracy <chantracyjames@gmail.com>
2026-08-08 17:59:45 +0800

    C++: Saturday Commit: Organized and alphabetically sorted headers in "headers.cpp", inside "src/learning/".

--- cpp/src/learning/headers.cpp
+++ cpp/src/learning/headers.cpp
@@ -3,19 +3,16 @@
     - required to use features
         - like std::cout, std::cin, std::strings
     - libraries:
-        - iostream
-            - includes the input and output libraries
-            - std::cout for output
-            - std::cin for input
+        - algorithm
+            - includes the algorithm library
+            - enables the use of algorithm functions
             - syntax:
-                #include <iostream>
-        - string
-            - includes the string libraries
-            - enables the string functionality
-            - also enables string functions
-                - like .length, .at(), etc.
+                #include <algorithm>
+        - array
+            - includes the array library
+            - enables the use of array data structures
             - syntax:
-                #include <string>
+                #include <array>
         - cmath
             - includes the cmath libraries
             - enbales most math functionalities
@@ -23,85 +20,68 @@
                 - example: sqrt(), round(), etc. 
             - syntax:
                 #include <cmath>
+        - cstdlib
+            - includes random number generation
+            - enables the use of random functions
+            - syntax:
+                #include <cstdlib>
+        - ctime
+            - includes basic time and date libraries
+            - enables querying of current time
+            - syntax:
+                #include <ctime>
+        - deque
+            - includes the deque library
+            - enables the use of deque data structures
+            - syntax:
+                #include <deque>
         - fstream
             - includes the file manipulation libraries
             - enables the use of file functions
                 - example: std::fstream, std::ofstream, std::ifstream
             - syntax:
                 #include <fstream>
-        - ctime
-            - includes basic time and date libraries
-            - enables querying of current time
+        - iostream
+            - includes the input and output libraries
+            - std::cout for output
+            - std::cin for input
             - syntax:
-                #include <ctime>
-        - cstdlib
-            - includes random number generation
-            - enables the use of random functions
-            - syntax:
-                #include <cstdlib>
-            - to generate random numbers
-                - it is done using the rand() function
-                - example:
-                    // generate a number from 0 - 100
-                    int myNum = rand() % 101;
-                - sidenote:
-                    - thie method of randon number generation only works one
-                        - it will only randomize the number once
-                        - meaning, it will be the same the same in-between program runs
-                    - to solve this, the srad() and time() function is used
-                        - it makes the number generation a lot more randomized
-                        - example:
-                            // inside a function
-                            srand(time(0));
-                            int myNum = rand() % 101;
-        - set
-            - includes the set library
-            - enables the use of set data structures
-            - syntax:
-                #include <set>
-        - algorithm
-            - includes the algorithm library
-            - enables the use of algorithm functions
-            - syntax:
-                #include <algorithm>
+                #include <iostream>
         - list
             - includes the list library
             - enables the use of list data structures
             - syntax:
                 #include <list>
-        - vector
-            - includes the vector library
-            - enables the use of vector data structures
-            - syntax:
-                #include <vector>
-        - stack
-            - includes the stack library
-            - enables the use of stack data structures
-            - syntax:
-                #include <stack>
-        - queue
-            - includes the queue library
-            - enables the use of queue data structures
-            - syntax:
-                #include <queue>
-        - deque
-            - includes the deque library
-            - enables the use of deque data structures
-            - syntax:
-                #include <deque>
-        - set
-            - includes the set library
-            - enables the use of set data structures
-            - syntax:
-                #include <set>
         - map
             - includes the map library
             - enables the use of map data structures
             - syntax:
                 #include <map>
-        - array
-            - includes the array library
-            - enables the use of array data structures
+        - queue
+            - includes the queue library
+            - enables the use of queue data structures
             - syntax:
-                #include <array>
+                #include <queue>
+        - set
+            - includes the set library
+            - enables the use of set data structures
+            - syntax:
+                #include <set>
+        - stack
+            - includes the stack library
+            - enables the use of stack data structures
+            - syntax:
+                #include <stack>
+        - string
+            - includes the string libraries
+            - enables the string functionality
+            - also enables string functions
+                - like .length, .at(), etc.
+            - syntax:
+                #include <string>
+        - vector
+            - includes the vector library
+            - enables the use of vector data structures
+            - syntax:
+                #include <vector>
 */
