0151c85ad01f00e4ce88a8b3b28e5b293eea814f
tracy <chantracyjames@gmail.com>
2026-08-15 06:31:08 +0800

    Python: Saturday Commit: Updated "database_stuff.py", inside "src/exercises/" with more features.

--- python/src/exercises/database_stuff.py
+++ python/src/exercises/database_stuff.py
@@ -18,7 +18,7 @@ def main() -> None:
 
         while True:
             menu_stuff()
-            process_input(int(input()), db_stuff)
+            process_input(input(), db_stuff)
 
     except mysql.connector.Error as err:
         print(f"debug it: {err}")
@@ -28,27 +28,27 @@ def main() -> None:
 
 
 def menu_stuff():
-    print('Stupid tarcy\'s Database ("0" is exit)')
-    print("-------------------------------------")
+    print("Stupid tarcy's Database")
+    print("-----------------------")
     print("1. List table contents")
     print("2. Add new record")
     print("3. Delete record")
-    print("-1. Advanced")
+    print("x. Advanced")
     print("")
     print("Input: ", end="")
 
 
 def process_input(user_input: int, db: MySQLConnection):
     match user_input:
-        case 1:
+        case "1":
             list_stuff(db)
-        case 2:
+        case "2":
             add_stuff(db)
-        case 3:
+        case "3":
             del_stuff(db)
-        case -1:
+        case "x":
             advanced_stuff(db)
-        case 0:
+        case "0":
             exit(0)
         case _:
             print(f"Invalid input: {user_input}")
@@ -56,11 +56,12 @@ def process_input(user_input: int, db: MySQLConnection):
 
 def list_stuff(db: MySQLConnection) -> None:
     temp_cursor: MySQLCursor = db.cursor()
-    temp_cursor.execute("SELECT * FROM programming_view ORDER BY EntryNumber")
-    print(f"\n{'Entry'} : {'Language':^12s}")
-    print("---------------------")
-    for entry, lang in temp_cursor:
-        print(f"{entry:^5d} : {lang:^12s}")
+    temp_cursor.execute("SELECT * FROM db_table ORDER BY ID")
+    print("ID : Language Name : Current Level")
+    for x in temp_cursor:
+        print(f"{x[0] if x[0] is not None else "NULL":^2} : ", end="")
+        print(f"{x[1] if x[1] is not None else "NULL":^13} : ", end="")
+        print(f"{x[2] if x[2] is not None else "NULL":^13} : ", end="")
     print()
 
     temp_cursor.close()
@@ -68,9 +69,10 @@ def list_stuff(db: MySQLConnection) -> None:
 
 def add_stuff(db: MySQLConnection) -> None:
     temp_cursor: MySQLCursor = db.cursor()
-    temp_in: str = input("\nEnter new Language: ")
-    new_stuff: str = "INSERT INTO programming (LanguageName) VALUES (%s)"
-    new_entry: tuple[str] = (temp_in,)
+    temp_in_lang: str = input("\nEnter new Language: ")
+    temp_in_lev: str = input("\nEnter Current Level: ")
+    new_stuff: str = "INSERT INTO db_table (LanguageName, CurrentLevel) VALUES (%s, %s)"
+    new_entry: tuple[str] = (temp_in_lang, temp_in_lev)
 
     temp_cursor.execute(new_stuff, new_entry)
     db.commit()
@@ -92,33 +94,52 @@ def del_stuff(db: MySQLConnection) -> None:
 
 def advanced_stuff(db: MySQLConnection) -> None:
     advanced_menu_stuff()
-    process_advanced_input(int(input()), db)
+    process_advanced_input(input(), db)
 
 
 def advanced_menu_stuff() -> None:
-    print('Advanced stuff here ("0" means go back)')
-    print("---------------------------------------")
+    print("Advanced stuff here")
+    print("-------------------")
     print("1. Create Table")
-    print("2. Truncate Table")
-    print("3. Delete Table")
+    print("2. Create View")
+    print("3. Truncate Table")
+    print("r. Basic Menu")
     print("")
     print("Input: ", end="")
 
 
-def process_advanced_input(user_input: int, db: MySQLConnection):
+def process_advanced_input(user_input: int, db: MySQLConnection) -> None:
     match user_input:
-        case 1:
+        case "1":
+            create_stuff(db)
+        case "2":
+            view_stuff(db)
+        case "0":
             ...
-        case 2:
-            ...
-        case 3:
-            ...
-        case -1:
-            ...
-        case 0:
+        case "r":
             ...
         case _:
             print(f"Invalid input: {user_input}")
+
+
+def create_stuff(db: MySQLConnection) -> None:
+    temp_cursor: MySQLCursor = db.cursor()
+    temp_cursor.execute(
+        "CREATE TABLE IF NOT EXISTS db_table ( ID int AUTO_INCREMENT PRIMARY KEY, LanguageName varchar(255) NOT NULL, CurrentLevel varchar(255) NOT NULL, UNIQUE (LanguageName))"
+    )
+
+    db.commit()
+    print(f"Table has been created")
+
+
+def view_stuff(db: MySQLConnection) -> None:
+    temp_cursor: MySQLCursor = db.cursor()
+    temp_cursor.execute("SELECT * FROM db_table")
+
+
+def truncate_stuff(db: MySQLConnection) -> None:
+    temp_cursor: MySQLCursor = db.cursor()
+    temp_cursor.execute("TRUNCATE TABLE db_stuff")
 
 
 if __name__ == "__main__":
