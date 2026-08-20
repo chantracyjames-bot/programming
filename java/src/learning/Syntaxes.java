a46438f464cd70a3b0a8625cbea59ec041445b3b
tracy <chantracyjames@gmail.com>
2026-08-20 21:02:34 +0800

    Java: Thursday Commit: Moved concepts from "src/learning/General.java" to "src/learning/Syntaxes.md". Moved sample code from "General.java" into "Syntaxes.java", inside of "src/learning". Removed "src/learning/General.java" from the repository.

---
+++ java/src/learning/Syntaxes.java
@@ -0,0 +1,15 @@
+public class Syntaxes // the file name matches the class name
+{
+    public static void main(String args[])
+    // main() signifies the start of the program
+        // it is the entry point for the program to start in
+    {
+        System.out.println("Hello World");
+        // System.out.print() sends text to output in the terminal or screen
+            // System is a built-in Java class
+            // out is a member of the System class
+            // print() is a method used for printing
+        // statement need to end with a semicolon to avoid errors
+    }
+    // basic Java program
+}
