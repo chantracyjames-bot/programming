f0719827b03591fb97217958e4a24d32f2533024
tracy <chantracyjames@gmail.com>
2026-08-25 19:51:35 +0800

    Java: Tuesday Commit: Added some documentation for how the code works, making it easier to understand. File "BasicCalculator.java" was modified from "project/app/src/main/java/tracy/calc/".

--- java/project/app/src/main/java/tracy/calc/BasicCalculator.java
+++ java/project/app/src/main/java/tracy/calc/BasicCalculator.java
@@ -11,6 +11,7 @@ public class BasicCalculator extends Calculator {
         // easter egg
         // got bored and did some stupid stuff
         if (getFirstNumber() == 10000 && getSecondNumber() == 10000) {
+            // just ignore this part
             setResult(8);
             idkman.mrkrabs();
             return;
@@ -25,14 +26,17 @@ public class BasicCalculator extends Calculator {
 
     // divides stuff
     public void divideNumbers() {
+        // runs if the second number IS NOT zero
         if (!(getSecondNumber() == 0.0)) {
             setResult(getFirstNumber() / getSecondNumber());
             return;
-        } setResult(0.0);
+        } setResult(0.0); // if the second number IS zero
     }
 
     // modulo stuff
     public void moduloNumbers() {
+        // ignore this
+        // some stupid easter egg which i thought is funny
         if (getSecondNumber() == 0) {
             setResult(-1);
             return;
