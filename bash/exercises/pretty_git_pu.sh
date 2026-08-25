8a55e275b178f45ade327376ce06c594947e5be4
tracy <chantracyjames@gmail.com>
2026-08-25 20:58:30 +0800

    Bash: Tuesday Commit: Updated "pretty_git_pu.sh" to have a forced push mode, also updated how arguments are handled. Source file is located at "exercises/".

--- bash/exercises/pretty_git_pu.sh
+++ bash/exercises/pretty_git_pu.sh
@@ -2,13 +2,24 @@
 
 DIRECTORY="$HOME/Documents/Programming"
 
-echo "======== Pushing to GitHub ========" 
-git push github main
-echo "======== Pushing to GitLab ========" 
-git push gitlab main
-echo "============= FINISHED ============"
+if [[ "${1}" == "-f" ]]; then
+    echo "=== Pushing to GitHub (forced) ===" 
+    git push -f github main
+    echo "=== Pushing to GitLab (forced) ===" 
+    git push -f gitlab main
+    echo "============ FINISHED ============"
+elif [[ "${1}" == "-n" ]]; then
+    echo "======== Pushing to GitHub ========" 
+    git push github main
+    echo "======== Pushing to GitLab ========" 
+    git push gitlab main
+    echo "============= FINISHED ============"
+else
+    echo "No options selected, use \"-n\" for a normal push or \"-f\" for a forced push."
+    exit 1
+fi
 
-if [[ $(git config list | grep "remote.github.url") == "remote.github.url=git@github.com:chantracyjames-bot/programming.git" ]]; then
+if [[ "$(git config list | grep "remote.github.url")" == "remote.github.url=git@github.com:chantracyjames-bot/programming.git" ]]; then
     # such an idiot
     cd "$DIRECTORY" || exit
     export $(cat .env | xargs)
