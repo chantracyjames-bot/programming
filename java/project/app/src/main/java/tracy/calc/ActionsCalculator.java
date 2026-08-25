92d5735548388216ab1f581d42e7a7ae35130d39
tracy <chantracyjames@gmail.com>
2026-08-25 20:00:02 +0800

    Java: Tuesday Commit: Renamed "ActionsCalculaor.java" to "ActionsCalculator.java", fixing a typo in the naming of the file. Added some documentation for how the code works, making it easier to understand. File "ActionsCalculator.java" was modified from "project/app/src/main/java/tracy/calc/".

--- java/project/app/src/main/java/tracy/calc/ActionsCalculator.java
+++ java/project/app/src/main/java/tracy/calc/ActionsCalculator.java
@@ -4,12 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JTextField;
 
-public class ActionsCalculaor implements ActionListener {
+// abstraction and inheritance
+public class ActionsCalculator implements ActionListener {
+    // encapsulation
     private BasicCalculator bc;
     private JTextField in;
 
     // constructor
-    public ActionsCalculaor(BasicCalculator bc, JTextField in) {
+    public ActionsCalculator(BasicCalculator bc, JTextField in) {
         this.bc = bc;
         this.in = in;
     }
@@ -24,15 +26,21 @@ public class ActionsCalculaor implements ActionListener {
         switch (command) {
             // arithmetic operations
             case "+", "-", "*", "/", "%" -> {
+                // if the input is not empty
                 if (!in.getText().isEmpty()) {
+                    // grabs the input from the screen
+                    // places it in bc.firstNum (private)
                     bc.setFirstNumber(Double.parseDouble(in.getText()));
+                    // sets the operator
                     bc.setOperator(command.charAt(0));
+                    // gets ready for the second number
                     in.setText("");
                 }
             }
                 
             // adds a decimal point
             case "." -> {
+                // if there is no dot in the screen, adds one
                 if (!currentText.contains(".")) {
                     in.setText(in.getText() + ".");
                 }
@@ -40,6 +48,8 @@ public class ActionsCalculaor implements ActionListener {
 
             // flips the sign of the value on screen
             case "+/-" -> {
+                // if the input is not empty
+                // flips the sign of the current number
                 if (!in.getText().isEmpty()) {
                     Double currentNumber = -1 * Double.parseDouble(in.getText());
                     in.setText(Double.toString(currentNumber));
@@ -50,13 +60,18 @@ public class ActionsCalculaor implements ActionListener {
             case "=" -> {
                 // easter egg
                 // the original stupid ideaz
+                // ignore this
                 if (in.getText().equals("Hello World")) {
                     idkman.tarcySux();
                 }
 
+                // if the input is not empty and the operator is also not empty
                 if (!in.getText().isEmpty() && (bc.getOperator() != ' ')) {
+                    // places the number on the screen to be in bc.secondNum (private)
                     bc.setSecondNumber(Double.parseDouble(in.getText()));
                     
+                    // basic switch case to determice which operator to use
+                    // used a getter to get bc.operator (private)
                     switch (bc.getOperator()) {
                         case '+' -> bc.addNumbers();
                         case '-' -> bc.subtractNumbers();
@@ -64,21 +79,27 @@ public class ActionsCalculaor implements ActionListener {
                         case '/' -> bc.divideNumbers();
                         case '%' -> { 
                             bc.moduloNumbers();
+                            // ignore this, some stupid easter egg
                             if (bc.getResult() == -1) {
                                 in.setText("Hello World");
                             }
                         }
                     }
-                    if (bc.getResult() != -1) {
-                        in.setText(String.valueOf(bc.getResult()));
-                    }
+
+                    // displays the result
+                    in.setText(String.valueOf(bc.getResult()));
+                    // sets the operator to be empty again
                     bc.setOperator(' ');
+                    // sets this value to be true
+                    // pressing any numbers after pressing the "=" button
+                    // it clears everything and then displays the new number
                     bc.setAfterCalc(true);
                 }
             }
 
             // clears the screen
             case "C" -> {
+                // clears everything
                 in.setText("");
                 bc.setFirstNumber(0);
                 bc.setSecondNumber(0);
@@ -88,18 +109,20 @@ public class ActionsCalculaor implements ActionListener {
 
             // backspace stuff
             case "D" -> {
+                // deletes the later number if the input is not empty
                 if (!in.getText().isEmpty()) {
                     in.setText(currentText.substring(0, currentText.length() - 1));
                 }
             }
 
-            // if any of the buttons are clicked
+            // if the user pressed the NUMBER buttons
             default -> {
+                // afterCalc is used here, clearing the number if the value is true
                 if (bc.getAfterCalc()) {
                     bc.setAfterCalc(false);
                     in.setText("");
-                    System.out.println("whyyyyy");
                 }
+                // sets the number
                 in.setText(in.getText() + command);
             }
         }
