20825cfa6ddfea34a20bb50ef3c2ef73e70b00a0
tracy <chantracyjames@gmail.com>
2026-09-01 10:51:48 +0800

    C: Tuesday Commit: Moved folders "src/exercises/", "src/learning/", and "src/practice/" to "src/sandbox/" for better organization. Moved folder's corresponding files to their new locations.

--- c/src/practice/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-int main() {
-    printf("Choose your number from 1-10.");
-    int choice;
-    scanf("%d", &choice);
-    processNumber(loudPrinter, choice);
-    processNumber(quietPrinter, choice);
-    return 0;
-}
