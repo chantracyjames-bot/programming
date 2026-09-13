54f3cbef9781a7c2c465c14a8dc52b68b55317cd
tracy <chantracyjames@gmail.com>
2026-09-13 19:24:22 +0800

    feat(python): Sunday Commit - Added sample code
    Added some sample code for `src/learning/modules.py`

---
+++ python/src/learning/modules.py
@@ -0,0 +1,8 @@
+# imports a single package
+import github
+
+# imports a subpackage from a bigger package
+from github import Repository
+
+# imports all subpackages from a bigger package
+from github import *
