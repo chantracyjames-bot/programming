ccaf7d50f6abdc3095ae74b5070ce2705c98c50e
tracy <chantracyjames@gmail.com>
2026-09-13 12:45:23 +0800

    docs(sql): Sunday Commit - Removed documentations
    Moved documentations from `sql/learning/operartors/exists.sql` into `/p_notes/sql/operators.md`.

--- sql/learning/operators/exists.sql
+++ sql/learning/operators/exists.sql
@@ -1,23 +1,9 @@
-/* EXISTS operator
-    - used to check whether a subquery returns any rows
-        - commonly used in a WHERE clause
-    - evaluates to TRUE if the subquery returns at least one row
-        - returns FALSE otherwise
-    - can be used with the NOT operator
-*/
-
 USE test;
 
--- SELECT <column/s>
--- FROM <table_name>
--- WHERE EXISTS (<subquery>);
-    -- EXISTS syntax
+-- EXISTS
 SELECT * FROM yippie
 WHERE EXISTS (SELECT yes from yippie);
 
--- SELECT <column/s>
--- FROM <table_name>
--- WHERE NOT EXISTS (<subquery>);
-    -- NOT EXISTS syntax
+-- NOT EXISTS
 SELECT * FROM yippie
 WHERE NOT EXISTS (SELECT yes from yippie);
